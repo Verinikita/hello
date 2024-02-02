98327dd00a09e5b5b4be7390663f4201a029a204
Verinikita <80643476+Verinikita@users.noreply.github.com>
2024-02-02 16:32:22 -0300

    Update user.py

--- api/user.py
+++ api/user.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
-from os.path import join
+from os.path import dirname, abspath, join
+dir = dirname(abspath(__file__))
  
 class handler(BaseHTTPRequestHandler):
  
@@ -7,7 +8,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        with open(join('data', 'file.txt'), 'r') as file:
+        with open(join(dir, '..', 'data', 'file.txt'), 'r') as file:
           for line in file:
             self.wfile.write(line.encode())
         return
